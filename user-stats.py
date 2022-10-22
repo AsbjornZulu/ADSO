1cd4f116af279fafaa1f32769ec2cb105cb1f36d
AsbjornZulu <51274631+AsbjornZulu@users.noreply.github.com>
2022-10-22 21:19:24 +0000

    Add files via upload

---
+++ user-stats.py
@@ -0,0 +1,62 @@
+import subprocess as sp
+import sys
+ 
+#Defimos una funcion encargada de enviar comandos para facilitar la programacion en Python.
+def run_command(command):
+    process = sp.Popen(command, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
+    retcode = process.wait()
+    if retcode != 0:
+        raise Exception("Problem running command: " + command)
+    stdout, stderr = process.communicate()
+    return stdout.decode('UTF-8').rstrip()
+ 
+ 
+print("Resum de logins:")
+ 
+#Splitlines divide el string en elemenos para facilitaros porder iterarla
+users = run_command("cat /etc/passwd | cut -d : -f1").splitlines()
+ 
+for user in users:
+    numlogs=0
+    days=0
+    hours=0
+    minutes=0
+ 
+    logins = run_command("last -F "+user+" | grep '(' | cut -d '(' -f2 | cut -d ')' -f1").splitlines()
+ 
+    for login in logins:
+        numlogs = numlogs + 1
+        day = run_command("echo "+login+" | grep + | cut -d + -f1")
+        if( day != ""):
+                days = days + int(day)
+ 
+        hour = run_command("echo "+login+" | cut -d + -f2 | cut -d : -f1")
+        if( hour != ""):
+            try:
+                hours = hours + int(hour)
+            except: 
+                print(hour)
+        minute = run_command("echo "+login+" | cut -d ':' -f2")
+        if( minute != ""):
+            try:
+                minutes = minutes + int(minute)
+            except: 
+                print(minute) 
+        
+    #print(minutes, hours, days)
+    minutes = minutes + ( (hours*60) )
+    if(minutes > 0):
+        print("Usuari "+user+": temps total de login ",minutes," min, nombre total de logins: ",numlogs)
+ 
+ 
+print("\nResum d'usuaris connectats:")
+users = run_command("cat /etc/passwd | cut -d: -f1").splitlines()
+ 
+for user in users:
+        numProcesos = run_command("ps aux | grep "+user+" | wc -l")
+        cpu=run_command("ps aux | grep "+user+"| awk 'NR>2{arr[1]+=$3}END{for(i in arr) print arr[1]}'")
+        try:
+            if( int(cpu) != 0):
+                print("Usuari "+user+": "+numProcesos+" processos -> "+cpu+"% CPU")
+        except:
+            print
